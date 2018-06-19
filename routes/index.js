36625a9dd702cecd776f94c2d9508bb7ffa7175d
Eleyni Rodriguez <eleyni.m.rodriguez@gmail.com>
2018-06-19 00:26:46 -0400

    Sets up song request model and basic routing.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,62 @@
 const express = require('express');
 const router  = express.Router();
+const Song = require('../models/song')
 
+// Home Page
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+// Schedule Page
+router.get('/schedule', (req, res, next) => {
+  res.render('schedule');
+});
+
+
+// Travel Page
+router.get('/travel', (req, res, next) => {
+  res.render('travel');
+});
+
+
+// Bridal Party
+router.get('/entourage', (req, res, next) => {
+  res.render('entourage');
+});
+
+
+// Song Request Page
+// router.get('/song-request', (req, res, next) => {
+//   res.render('songs');
+// });
+
+// ADD new request through Postman
+router.post('/song-request', (req, res, next) => {
+  const newSong = {
+    song: req.body.song,
+  }
+  Task.create(newSong)
+  .then((requestJustCreated) => {
+    res.json(requestJustCreated)
+  })
+  .catch((err) => {
+    res.json(err)
+  })
+})
+
+
+// Gallery Page
+router.get('/gallery', (req, res, next) => {
+  res.render('gallery');
+});
+
+
+// FAQ Page
+router.get('/faq', (req, res, next) => {
+  res.render('faq');
+});
+
 
 module.exports = router;
